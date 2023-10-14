031715964467fd6a8a2fa36ee726cab577841df2
Ebuka Peter <onyebinamma@systemspecs.com.ng>
2023-10-14 17:51:24 +0100

    implement the hero section

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import {NavigationBarComponent} from "../components/navigation-bar/navigation-bar.component";
+import {HeroSectionComponent} from "../components/hero-section/hero-section.component";
 
 
 
 @NgModule({
   declarations: [
     LandingComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HeroSectionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class LandingModule { }
